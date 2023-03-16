69aec0832bdf3e3497e59701de9866b91dc3a8cf
Venetius Ohanyere <junohanyere@gmail.com>
2023-03-16 05:55:59 -0700

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main (void)
+int main(void)
 
 {
 
-	puts ("\"Programming is like building a multiligual puzzle");
+	puts("\"Programming is like building a multiligual puzzle");
 return 0;
 
 }
