160c85d41696acbb0ffa30b62934e5ad97900d1c
Venetius Ohanyere <junohanyere@gmail.com>
2023-03-16 06:06:00 -0700

    Fixed 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,10 +10,8 @@
 */
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multiligual puzzle");
 return (0);
-
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+THis is a repository about low level programming, to demonstrate the functionalities of low level languages
