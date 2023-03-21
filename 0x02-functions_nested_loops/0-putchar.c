8261b2879418ff7e6e30c87c0746974d624c964f
Venetius Ohanyere <junohanyere@gmail.com>
2023-03-21 07:12:52 -0700

    resolved 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - Entry point
- *
- * Description: prints _putchar using putchar prototype
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
 */
 
-int main(void)
+int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
+	char str[] = "_putchar";
+	int ch;
 
-	for (ch = 0; ch < 8; ++ch)
+	for (ch = 0; ch < 8; ++ch)
 		_putchar(str[ch]);
 	_putchar('\n');
 
-	return (0);
+	return (0);
 }
