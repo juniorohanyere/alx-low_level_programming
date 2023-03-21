9894d529310d89083839cbf751dd08c07bec1ded
Venetius Ohanyere <junohanyere@gmail.com>
2023-03-21 15:29:18 -0700

    resolved 0-putchar, betty coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,19 +6,21 @@
  * Description: Printing _putchar
  *
  * Return: Always return 0
- */
+*/
 
 int main(void)
 
 {
-        _putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-        return (0);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
