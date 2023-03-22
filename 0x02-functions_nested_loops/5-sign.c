e04e51bb3e7b535ed0257ba1d7e1ee94fcb3377a
Venetius Ohanyere <junohanyere@gmail.com>
2023-03-21 18:19:20 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,7 @@
-#include"main.h"
+#includ e"main.h"
 
 /**
  * print_sign - print + if n is greater than zero,
-
  *             0 if n is zero and - if n is less
  *             than zero.
  *
@@ -12,20 +11,31 @@
 */
 
 int print_sign(int n)
+
 {
-        if (n > 0)
-        {
-                _putchar(43 + 0);
-                return (1);
-        }
-        else if (n == 0)
-        {
-                _putchar(0 + 48);
-                return (0);
-        }
-        else
-        {
-                _putchar(45 + 0);
-                return (-1);
-        }
+	if (n > 0)
+
+	{
+
+		_putchar(43 + 0);
+		return (1);
+	}
+
+	else if (n == 0)
+
+	{
+		_putchar(0 + 48);
+		return (0);
+
+	}
+
+	else
+
+	{
+		_putchar(45 + 0);
+
+		return (-1);
+
+	}
+
 }
