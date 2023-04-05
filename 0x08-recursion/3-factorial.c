379f3fae9499f894421c6dcaa715bf2d461e8f26
Twin J <junohanyere@gmail.com>
2023-04-05 15:38:21 +0100

    Factorial of a number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+/**
+ * factorial - function to return
+ *		the factorial of a given number
+ *
+ * @n: Integer to factorialize
+ *
+ * Return: Return -1 if n < 0, otherwise return 0
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n-1);
+}
