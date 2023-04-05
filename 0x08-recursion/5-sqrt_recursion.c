20028d7a6f3d408a0423473805bb34bce5e7f9a1
Twin J <junohanyere@gmail.com>
2023-04-05 17:59:05 +0100

    Resolved

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,27 +3,19 @@
 /**
  * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
  *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
+ * @n: input number
+ * @i: iterator
  *
  * Return: square root of @n or -1
 */
-int _sqrt_recursion_wrapper(int n, int min, int max)
+
+int _sqrt(int n, int i)
 {
-	int guess, guess_squared;
-
-	guess = (min + max) / 2;
-	guess_squared = guess * guess;
-
-	if (guess_squared == n)
-		return (guess);
-	else if (min == max)
+	if (i * i > n)
 		return (-1);
-	else if (guess_squared < n)
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
 }
 
 /**
@@ -37,12 +29,7 @@ int _sqrt_recursion_wrapper(int n, int min, int max)
 
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
-		return (1);
-	else if (n == 0)
-		return (0);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+	return (_sqrt(n, 0));
 }
