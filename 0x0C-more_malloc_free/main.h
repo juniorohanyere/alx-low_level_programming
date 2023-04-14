1e9205580236ee48e2f9311762aefd3863ff4b0e
Twin J <junohanyere@gmail.com>
2023-04-14 20:05:35 +0100

    Multiply two numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,12 +8,9 @@ char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-<<<<<<< HEAD
 int is_digit(char *s);
 int _strlen(char *s);
 void errors(void);
-=======
 void multiply(char *s1, char *s2);
->>>>>>> 7ff9ff9632e1fa5834d0c2ea8aa5dfb6eda3db4a
 
 #endif	/* MAIN_H */
