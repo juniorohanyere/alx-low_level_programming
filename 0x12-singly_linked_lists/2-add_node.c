dfa7b4cfe9b5597d4590a0d791d9abc16f2c49a1
Twin J <junohanyere@gmail.com>
2023-04-27 15:07:11 +0100

    add new node to a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -25,9 +25,8 @@ list_t *add_node(list_t **head, const char *str)
 		new_node->next = NULL;
 	else
 		new_node->next = *head;
-		new_node->str = strdup(str);
-		new_node->len = str_len;
-		*head = new_node;
-
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	*head = new_node;
 	return (*head);
 }
