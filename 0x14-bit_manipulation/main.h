5bfa2b5eaa9bf1297c0d71a06c5ea4491cbc4fcc
Twin J <junohanyere@gmail.com>
2023-05-04 12:44:24 +0100

    modified

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 unsigned int get_length(const char *str);
 unsigned int convert_to_int(char c);
+void print_binary(unsigned long int n);
+int get_len(unsigned long int num);
 
 #endif	/* MAIN_H */
