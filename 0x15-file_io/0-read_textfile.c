1e99bc4619697872f16a751cdcb66a66d0a93600
Twin J <junohanyere@gmail.com>
2023-05-08 17:47:18 +0100

    read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file
+ *		   and prints it to the POSIX standard output
+ *
+ * @filename: the4 text file to read from
+ * @letters: the number of letters to read and print
+ *
+ * Return: return 0 on failure
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t rfile, i;
+	char *wfile;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	wfile = malloc(sizeof(char) * letters);
+	if (wfile == NULL)
+	{
+		free(wfile);
+		return (0);
+	}
+	rfile = read(file, wfile, letters);
+	if (rfile == -1)
+		return (0);
+	i = write(STDOUT_FILENO, wfile, rfile);
+	if (i == -1 || rfile != i)
+		return (0);
+	free(wfile);
+	return (i);
+}
