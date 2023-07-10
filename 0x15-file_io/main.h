7d4f33529d5b9250ff5ee50872b64ad44423dacf
Twin J <junohanyere@gmail.com>
2023-07-10 15:20:52 +0100

    copy content of a file to another

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _err(int fd, int num, char *str);
+void _cp(int num, char *str[]);
 
 #endif	/* MAIN_H */
