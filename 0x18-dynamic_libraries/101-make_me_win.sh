cfa803d22ed6ad8067592bd831d8ba11562864b4
Twin J <junohanyere@gmail.com>
2023-08-21 20:02:58 +0100

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P . https://github.com/alx-tools/0x18.c/raw/master/gm
-export LD_PRELOAD=gm
+wget -q -O /tmp/101-make_me_win.so https://github.com/juniorohanyere/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-make_me_win.so
+export LD_PRELOAD=/tmp/101-make_me_win.so
