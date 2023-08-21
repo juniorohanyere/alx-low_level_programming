ae6b7252d08578ca3e916b4fbb92578a4fe08d7a
Twin J <junohanyere@gmail.com>
2023-08-21 16:33:04 +0100

    calling c functions in a python program

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,71 @@
+/**
+ * add - function to sum two input integers
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: return the sum of @a and @b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function to subtract
+ *	    one input integer from the other
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: return the subtraction of @b from @a
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function to multiply two input integers
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: return the product of @a and @b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function to divide two input integers
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: return the division of @a by @b
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+
+}
+
+/**
+ * mod - function to get the remainder
+ *	    of the division between two input integers
+ *
+ * @a: first input integer
+ * @b: second input integer
+ *
+ * Return: return the remainder of the division
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
