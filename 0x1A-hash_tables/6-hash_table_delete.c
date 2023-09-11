ad534ab6c6db6f32a9563b0c4dc90208d823e238
Twin J <junohanyere@gmail.com>
2023-09-11 11:37:56 +0100

    modified

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -14,10 +14,9 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_table_t *head;
+	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 
-	head = ht;
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
